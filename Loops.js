633f598db24527baee42ee6710d31bc4b77faa52
ScriptiveMen <i.satyakumarram@gmail.com>
2026-01-08 14:55:06 +0530

    loops done

---
+++ Loops.js
@@ -0,0 +1,19 @@
+// for loop
+
+for (let i = 0; i < 10; i++) {
+    if (i == 5) {
+        break;
+    }
+    console.log(i);
+}
+
+// let i = 1;
+// while (i < 10) {
+//     console.log(i);
+//     i++;
+// }
+
+let i = 5;
+do {
+    console.log(i);
+} while (i < 0);
