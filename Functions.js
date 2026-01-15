ca3cde62998056e72ae01df18f03a57662d30779
ScriptiveMen <i.satyakumarram@gmail.com>
2026-01-15 14:32:36 +0530

    func done

--- Functions.js
+++ Functions.js
@@ -21,9 +21,9 @@ write();
 
 // fat arrow function
 
-let abcd = () => {
-    console.log("ABCD..");
-};
+// let abcd = () => {
+//     console.log("ABCD..");
+// };
 
 abcd();
 
@@ -51,15 +51,122 @@ AnimalDance("Dog");
 
 // add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-function add(a, b, c, ...val) {
-    console.log(a, b, c, val);
+// function add(a, b, c, ...val) {
+//     console.log(a, b, c, val);
+// }
+
+// console.log(1, 2, 3, 4, 5, 6);
+
+// function def(v) {
+//     return 33 + v;
+// }
+
+// let val = def(3);
+// console.log(val);
+
+// first class func
+
+// function abcd(val) {
+//     val();
+// }
+
+// abcd(function () {
+//     console.log("Hello");
+// });
+
+// hof
+
+// function def() {
+//     return function () {
+//         console.log("Hello");
+//     };
+// }
+
+// def()();
+
+// function def(val) {
+//     val();
+// }
+
+// def(function () {
+//     console.log("Nice..");
+// });
+
+// impure and pure functions
+
+let a = 34;
+
+// function abcd() {
+//     a++;
+// }
+
+// closures
+
+// function abcd() {
+//     let a = 19;
+//     return function () {
+//         console.log(a);
+//     };
+// }
+
+// lexical scoping
+
+// function abcd() {
+//     let a = 10;
+
+//     function defg() {
+//         let b = 20;
+
+//         function hijk() {
+//             let c = 30;
+//         }
+//     }
+// }
+
+// iife
+
+let lib = (function () {
+    let score = 0;
+
+    return {
+        getScore: function () {
+            console.log(score);
+        },
+        setScore: function (val) {
+            score = val;
+        },
+    };
+})();
+
+console.log(lib.getScore());
+lib.setScore(40);
+
+// hoisting
+
+// abcd();
+
+// let abcd = function () {
+//     console.log("heyy");
+// };
+
+// fat arrow function is not created here because it looses this context
+
+let obj = {
+    value: 42,
+    regular: function () {
+        return this.value;
+    },
+    arrow: () => this.value,
+};
+
+// hoisting and tdz
+
+greet(); // ✅ Works
+function greet() {
+    console.log("greeting..");
 }
 
-console.log(1, 2, 3, 4, 5, 6);
-
-function def(v) {
-    return 33 + v;
-}
-
-let val = def(3);
-console.log(val);
+greet(); // ❌ Error
+const greet = () => {
+    console.log("greeting..");
+};
